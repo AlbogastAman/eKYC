8ba9307d69cbffb0f51534a1e9f524ead4da9cb3
AlbogastAman <albogast.aman@gmail.com>
2026-02-10 15:27:23 +0000

    Env configs

--- api/server.js
+++ api/server.js
@@ -5,4 +5,7 @@ const mongoURI = process.env.MONGODB_URI_DEV;
 
 InitiateMongoServer(mongoURI);
 
-app.listen(process.env.PORT_API || 5000);
+let PORT =process.env.PORT_API||5000;
+app.listen(PORT,() => {
+    console.log(`🚀 eKYC Service running on port ${PORT}`);
+});
