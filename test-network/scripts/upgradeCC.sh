9b797a0aedb43121b2ef414ee4b3bdc92f9db8b3
AlbogastAman <albogast.aman@gmail.com>
2026-02-15 13:38:53 +0000

    Added chaincode upgrade script

--- test-network/scripts/upgradeCC.sh
+++ test-network/scripts/upgradeCC.sh
@@ -22,7 +22,7 @@ function setGlobals() {
   elif [ $ORG -eq 2 ]; then
     export CORE_PEER_LOCALMSPID="Org2MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/../organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
-    export CORE_PEER_MSPCONFIGPATH=${PWD}../organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
     export CORE_PEER_ADDRESS=localhost:9051
   fi
 }
