2e9cfcac794d911c45ea26a1bab934e904ad13fd
AlbogastAman <albogast.aman@gmail.com>
2026-02-15 13:56:49 +0000

    Added chaincode upgrade script

--- test-network/scripts/upgradeCC.sh
+++ test-network/scripts/upgradeCC.sh
@@ -68,8 +68,8 @@ peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameO
 echo "Step 7: Committing Definition..."
 # We use the TLS variables provided by envVar.sh for the peers
 peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA --channelID ${CHANNEL_NAME} --name ${CC_NAME} \
---peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
---peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
+--peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
+--peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
 --version ${CC_VERSION} --sequence ${CC_SEQUENCE}
 
 echo "----------------------------------------------------------"
