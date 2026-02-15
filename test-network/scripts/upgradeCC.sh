49fc84c84d705d999a337c59489d9f2bb25238fc
AlbogastAman <albogast.aman@gmail.com>
2026-02-15 14:00:40 +0000

    Added chaincode upgrade script

--- test-network/scripts/upgradeCC.sh
+++ test-network/scripts/upgradeCC.sh
@@ -3,8 +3,8 @@
 # 1. Configuration
 CC_NAME="eKYC"
 CC_SRC_PATH="../../chaincode/javascript/" 
-CC_VERSION="2.0"                       
-CC_SEQUENCE="2"                        
+CC_VERSION="2.1"                       
+CC_SEQUENCE="3"                        
 CHANNEL_NAME="mychannel"
 
 # Path setup for the test-network directory
