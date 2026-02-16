f7130826e62e410bf597fd8339a0b0847870c5aa
AlbogastAman <albogast.aman@gmail.com>
2026-02-16 12:44:20 +0000

    Bug fix

--- api/utils/registerOrg.js
+++ api/utils/registerOrg.js
@@ -35,7 +35,7 @@ async function main() {
     // Path to the Admin certificate for the specified org
     const certPath = path.resolve(
         __dirname, '..', 'test-network', 'organizations', 'peerOrganizations',
-        `${orgNum}.example.com`, 'users', `Admin@${orgNum}.example.com`,
+        `${orgName}.example.com`, 'users', `Admin@${orgName}.example.com`,
         'msp', 'signcerts', 'cert.pem'
     );
 
