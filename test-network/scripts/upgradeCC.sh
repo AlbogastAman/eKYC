d9701f188f59fd6b5759a6ada66da573c4b1e354
AlbogastAman <albogast.aman@gmail.com>
2026-02-25 14:49:52 +0000

    Caliper troubleshooting

--- test-network/scripts/upgradeCC.sh
+++ test-network/scripts/upgradeCC.sh
@@ -3,8 +3,8 @@
 # 1. Configuration
 CC_NAME="eKYC"
 CC_SRC_PATH="../../chaincode/javascript/" 
-CC_VERSION="2.7"                       
-CC_SEQUENCE="9"                        
+CC_VERSION="2.8"                       
+CC_SEQUENCE="10"                        
 CHANNEL_NAME="mychannel"
 
 # Path setup for the test-network directory
