db0248f35637d3799d6afd40a123293d5ddcb173
AlbogastAman <albogast.aman@gmail.com>
2026-02-25 21:11:04 +0000

    Set Env - CORE_PEER_GATEWAY_MAXCONCURRENTREQUESTS=2500 for peers -n

--- caliper-ekyc/workloads/anchorCredential.js
+++ caliper-ekyc/workloads/anchorCredential.js
@@ -23,7 +23,7 @@ class AnchorCredentialWorkload extends WorkloadModuleBase {
         this.txIndex++;
         // Combine worker ID and index for a globally unique DID
         const globalIndex = this.workerOffset + this.txIndex;
-        const did = `did:fabric:userr${globalIndex}`;
+        const did = `did:fabric:usr${globalIndex}`;
 
         const client = {
             did: did,
