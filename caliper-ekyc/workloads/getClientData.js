0859d61f9faa2550d79c90dc7bae9cf23c662fce
AlbogastAman <albogast.aman@gmail.com>
2026-03-03 16:48:47 +0000

    Benchmark fixes

--- caliper-ekyc/workloads/getClientData.js
+++ caliper-ekyc/workloads/getClientData.js
@@ -10,17 +10,17 @@ class GetClientDataWorkload extends WorkloadModuleBase {
 
     async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
         await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
-        
+
         // 1. Configuration from YAML
         this.runID = this.roundArguments.seed || 'FINAL_01';
         this.totalAssets = this.roundArguments.totalAssets || 1500;
-        
+
         // 2. Identity Mapping - The Fix for "No contracts found"
         // We prioritize the argument passed from the Benchmark YAML
-        this.invoker = this.roundArguments.invoker || 'FI2'; 
-        
+        this.invoker = this.roundArguments.invoker || 'FI2';
+
         // 3. Sharding Logic (Must match createClient.js)
-        this.laneSize = 100000; 
+        this.laneSize = 100000;
         this.assetsPerWorker = Math.floor(this.totalAssets / this.totalWorkers);
     }
 
@@ -30,10 +30,11 @@ class GetClientDataWorkload extends WorkloadModuleBase {
         // 4. Randomized Read Logic
         const randomWorkerLane = Math.floor(Math.random() * this.totalWorkers);
         const randomIndexInLane = Math.floor(Math.random() * this.assetsPerWorker) + 1;
-        
+
         // Construct the DID: did:fabric:usr_[Seed]_[GlobalIndex]
         const globalUniqueIndex = (randomWorkerLane * this.laneSize) + randomIndexInLane;
         const did = `did:fabric:usr_${this.runID}_${globalUniqueIndex}`;
+        const fields = ["name", "address"];
 
         // 5. Query Execution
         // Using 'evaluateTransaction' logic via readOnly: true
@@ -41,8 +42,8 @@ class GetClientDataWorkload extends WorkloadModuleBase {
             contractId: 'eKYC',
             contractFunction: 'getClientData',
             invokerIdentity: this.invoker, // Targeted Org2 Identity
-            contractArguments: [did],
-            readOnly: true 
+            contractArguments: [did, fields],
+            readOnly: true
         };
 
         return this.sutAdapter.sendRequests(request);
