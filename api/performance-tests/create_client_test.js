ecee440ab5f0c935abf0d9cd9101dfc140403f07
AlbogastAman <albogast.aman@gmail.com>
2026-03-06 00:45:40 +0000

    Create client test modified

--- api/performance-tests/create_client_test.js
+++ api/performance-tests/create_client_test.js
@@ -33,7 +33,7 @@ export default function () {
 
     const res = http.post(url, payload, params);
 
-    console.log("res####", res)
+    console.log("res####", res.body)
 
     const isSuccessful = check(res, {
         'is status 200': (r) => r.status === 200,
@@ -42,6 +42,7 @@ export default function () {
         },
     });
 
+    console.log("isSuccessful####", isSuccessful)
     // SUCCESS LOGIC: Add the ID to our list so we can use it in the GET test
     if (isSuccessful) {
         createdIds.push(`${idNumber}@${randomFI.name}`);
