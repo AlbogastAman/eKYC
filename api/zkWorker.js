5c87bae07d37cf4a3cceb7f59b682711a09d26ba
AlbogastAman <albogast.aman@gmail.com>
2026-03-13 11:25:23 +0000

    Optimization of VC verification endpoint

--- api/zkWorker.js
+++ api/zkWorker.js
@@ -7,15 +7,16 @@ const vKeyPath = path.join(__dirname, "./build/requirements_check_key.json");
 const vKey = JSON.parse(fs.readFileSync(vKeyPath));
 
 async function verify() {
-    console.log("Nafika hapa####")
+
     try {
         const { publicSignals, proof } = workerData;
-        
+
         // 2. MATH EXECUTION (CPU Intensive)
         const isValid = await snarkjs.groth16.verify(vKey, publicSignals, proof);
-        
+        console.log("isValid isValid####", isValid)
         parentPort.postMessage({ isValid });
     } catch (error) {
+        console.log("Nafika hapa####", error)
         parentPort.postMessage({ isValid: false, error: error.message });
     }
 }
